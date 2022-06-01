8a27ecb174b166d157c16f9757fac72cfd8f7eb0
toyolife881 <toyolife.island881@gmail.com>
2022-06-01 11:21:59 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   # ルートパスへのアクセスがあったら、messages_controllerのindexアクションを呼び出す
   root to: "messages#index"
+
+  resources :users, only: [:edit,:update]
 end
