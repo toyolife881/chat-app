bb442906435e85bdd173da5655462d47f91b9ebd
toyolife881 <toyolife.island881@gmail.com>
2022-06-02 10:58:58 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   # ルートパスへのアクセスがあったら、messages_controllerのindexアクションを呼び出す
-  root to: "messages#index"
+  # root to: "messages#index"
+  root to: "rooms#index"
 
   resources :users, only: [:edit,:update]
   resources :rooms, only: [:new,:create]
