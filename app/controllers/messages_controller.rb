4e83dcee7fa22c9de6cc8c8c850d170c46b7ea1a
toyolife881 <toyolife.island881@gmail.com>
2022-06-02 16:18:14 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    # @messages = Message.all
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    # チャットルームに紐づいたメッセージのインスタンスを生成
+    @message = @room.messages.new(message_params)
+    if @message.save 
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    # ログインしているユーザーのidと紐づいている、メッセージの内容contentを受け取れるように許可
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
+
 end
